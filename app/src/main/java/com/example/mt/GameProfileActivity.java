61a7812d7bf034a92037dbb306238e96be76e69e
Larry1149 <chunvoon1149@gmail.com>
2019-11-14 09:28:45 +0800

    Mid Term GG

---
+++ app/src/main/java/com/example/mt/GameProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.mt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameProfileActivity extends AppCompatActivity {
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_profile);
+    }
+}
