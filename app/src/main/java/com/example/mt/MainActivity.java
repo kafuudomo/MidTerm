d7c66695ff2eff79bdc92d2d02eb7c6ce5c4915c
kafuudomo <bocchidesu23@gmail.com>
2019-11-16 11:49:08 +0800

    MidTerm

--- app/src/main/java/com/example/mt/MainActivity.java
+++ app/src/main/java/com/example/mt/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.mt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.InputType;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -15,34 +17,46 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.security.MessageDigest;
 
 
 public class MainActivity extends AppCompatActivity {
     private EditText entUserName, entPassword;
     private Button btnLogin;
+    private String data;
 
 
-    String Username = "Larry";
-    String Password = SHA("ABCABC");
+    String username="jason@gmail.com";
+    String Password = SHA("abc12345");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+        new HttpRequest().execute();
         findview();
         Login();
     }
 
-    private void findview(){
+    private void findview() {
         entUserName = findViewById(R.id.UserName);
         entPassword = findViewById(R.id.Password);
         btnLogin = findViewById(R.id.btn_Login);
 
     }
 
-    private void Login(){
+    private void Login() {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,30 +64,29 @@ public class MainActivity extends AppCompatActivity {
                 String inputP = SHA(entPassword.getText().toString());
 
 
-                if (inputU.equals(Username) && inputP.equals(Password)){
-                    Intent g = new Intent(MainActivity.this,GamesActivity.class);
+                if (inputU.equals(username) && inputP.equals(Password)) {
+                    Intent g = new Intent(MainActivity.this, GamesActivity.class);
                     startActivity(g);
-                    Toast.makeText(MainActivity.this,"Login Success",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
                     finish();
-                }else{
-                    Toast.makeText(MainActivity.this,"invalid username or password",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
                 }
 
             }
         });
 
 
-
     }
 
-    private String SHA(String x){
+    private String SHA(String x) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-256");
             md.update(x.getBytes());
 
             byte[] digest = md.digest();
             StringBuffer sb = new StringBuffer();
-            for (byte b : digest){
+            for (byte b : digest) {
                 sb.append(String.format("%02x", b & 0xff));
             }
             return sb.toString();
@@ -82,5 +95,96 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private class HttpRequest extends AsyncTask<Void, Void, Void> {
 
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            try {
+                JSONObject responseObject = new JSONObject(data);
+                JSONArray accountsArray = responseObject.getJSONArray("accounts");
+
+                for (int i = 0; i <= accountsArray.length(); i++) {
+
+
+
+
+                }
+
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+// Will contain the raw JSON response as a string.
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                URL url = new URL("http://api.myjson.com/bins/arlt2");
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    data = null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    data = null;
+                }
+                data = buffer.toString();
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                data = null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+
+        }
+
+    }
 }
